575c3e4b605e9b5dc0cdffec235622b999f9e9e8
david_ferrari <ferraridavid.hub@gmail.com>
2025-04-30 13:55:50 +0200

    First commit

---
+++ src/test/java/coding/interview/florence/consulting/group/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package coding.interview.florence.consulting.group.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
